3c9a02948b2079884fa8501999c085131944f53e
charroux <benoit.charroux@free.fr>
2016-07-08 12:09:22 +0200

    multiple arguments to compute

--- src/main/compose/Code4.groovy
+++
@@ -1,6 +0,0 @@
-				
-
-receive event from input whose "event == 0"		
-compute code3 with event.value, data			// event doit etre le premier pour que l'aggregator fonctionne
-when "code3 terminates"
-send code3.result to database
