89812aecd06002bf16903968042013c547722b22
charroux <benoit.charroux@free.fr>
2016-07-15 22:21:10 +0200

    addind args for QoS

--- src/main/groovy/org/olabdynamics/compose/tools/Compile.groovy
+++ src/main/groovy/org/olabdynamics/compose/tools/Compile.groovy
@@ -18,22 +18,33 @@ class Compile implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		if(args.size() != 1){
-			throw new Exception("A compose source file (.groovy extension) should be in ./compose/source, put the file name (with no path) as a argument")
+		String[] arguments = args[0].split("\\s+")
+		
+		if(arguments.length != 2){
+			throw new Exception("A compose source file (.groovy extension) should be in ./compose/source, put the file name (with no path) as the first argument, true or false for the Quality Of Service as a second argumeent")
 		}
 		
 		def visitor = new ComposeCodeVisitor(context: context)
 		def myCL = new MyClassLoader(visitor: visitor)
 		
-		//def uri = new URI("file:///C:/Users/Charroux_std/Documents/projet/ExecAndShare/Compose/ComposeTools/ComposeTools/src/main/compose/Code4.groovy")
-		String pathToCode = "." + File.separator + "compose" + File.separator + "source" + File.separator + args[0]
+		String pathToCode = "." + File.separator + "compose" + File.separator + "source" + File.separator + arguments[0]
 		def script = myCL.parseClass(new GroovyCodeSource(new File(pathToCode)))
-		
-		//def xmlSpringContent = "./src/main/resources/xmlSpringContext.xml"
-		def xmlSpringContent = "." + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "xmlSpringContext.xml"
-		
-		def xmlGenerator = new XmlGenerator(xmlSpringContent: xmlSpringContent, instructions: visitor.instructions, aggregators: visitor.aggregators)
-		xmlGenerator.generate()
+
+		boolean QoS = Boolean.parseBoolean(arguments[1])
+			
+		if(QoS == true){
+	
+			def xmlSpringContent = "." + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "QoSSpringContext.xml"
+			def xmlGenerator = new XmlGenerator(xmlSpringContent: xmlSpringContent, instructions: visitor.instructions, aggregators: visitor.aggregators, composeEvents: false)
+			xmlGenerator.generate()
+	
+		} else {
+
+			def xmlSpringContent = "." + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "xmlSpringContext.xml"
+			def xmlGenerator = new XmlGenerator(xmlSpringContent: xmlSpringContent, instructions: visitor.instructions, aggregators: visitor.aggregators, composeEvents: true)
+			xmlGenerator.generate()
+			
+		} 
 		
 		def javaCodeGenerator = new JavaCodeGenerator(context: context, instructions: visitor.instructions)
 		javaCodeGenerator.generate()
