1b2303f38293f55087e61aaf5d1c16f2bfe402ce
charroux <benoit.charroux@free.fr>
2016-07-15 12:43:02 +0200

    adding json converter

---
+++ src/main/groovy/org/olabdynamics/compose/tools/xmlgenerator/ComposeCompilationException.groovy
@@ -0,0 +1,17 @@
+package org.olabdynamics.compose.tools.xmlgenerator
+
+class ComposeCompilationException extends Exception{
+
+	public ComposeCompilationException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ComposeCompilationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
